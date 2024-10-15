7301f1da7456cd1661f3eae7feb29b685a48318d
Alym Karim <alymkarimg@gmail.com>
2024-10-15 16:00:10 +0100

    fix: get components working

--- frontend/entrypoints/Header/Header.tsx
+++ frontend/entrypoints/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = ({ counter, test }) => {
     return (
     <div>
       <h1>Vite + React + Shopify</h1>
-      <h1>{test}</h1>
+      <h1>{test ? "true" : "false"}</h1>
       <p>{counter ?? 0}</p>
     </div>
 
